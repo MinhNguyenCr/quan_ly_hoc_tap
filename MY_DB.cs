f33e85434082c65c37ee843378c2b0c62344fc00
MinhNguyenCr <163228645+MinhNguyenCr@users.noreply.github.com>
2024-06-11 20:15:15 +0700

    Add files via upload

---
+++ MY_DB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DACKW
+{
+    internal class MY_DB
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-J6I016E\SQLEXPRESS05;Initial Catalog=DACK;Integrated Security=True;TrustServerCertificate=True");
+
+        // get the connection
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+
+        // open the connection
+        public void openConnection()
+        {
+            if ((con.State == ConnectionState.Closed))
+            {
+                con.Open();
+            }
+
+        }
+
+
+        // close the connection
+        public void closeConnection()
+        {
+            if ((con.State == ConnectionState.Open))
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
